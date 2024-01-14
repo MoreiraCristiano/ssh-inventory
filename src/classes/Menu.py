15e049ee255a57c145fbabf7a8d5d224e910ef2a
cristiano m <moreiracristiano702@gmail.com>
2024-01-13 21:56:04 -0300

    update comments

--- src/classes/Menu.py
+++ src/classes/Menu.py
@@ -103,7 +103,8 @@ class Menu:
 
     def collections(self):
         """
-        Description: Instantiates the InventoryHandle class, retrieves the names of all collections, and presents a menu with the names of the collections.
+        Description: Instantiates the InventoryHandle class, retrieves the names of all collections, and presents a menu 
+         with the names of the collections.
         Params:
         Return: The option choosed (Should be a collection name) or -1 if has no collection in database
         """
